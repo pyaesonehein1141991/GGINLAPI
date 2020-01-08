7b35b7c08afac31c2e19de7fd7ec566b4e6423fa
ျပည့္စံုဟိန္း <pyaesonehein1141991@gmail.com>
2020-01-08 10:27:12 +0630

    initial commit

---
+++ src/main/java/org/tat/gginl/api/GginlApiApplication.java
@@ -0,0 +1,13 @@
+package org.tat.gginl.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GginlApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GginlApiApplication.class, args);
+	}
+
+}
