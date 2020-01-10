38764b2202612ead4b90e3c38b47cd405fd4d205
Aung Htet Paing <aunghtetpaing@hotmail.com>
2020-01-10 09:04:04 +0630

    customer information

--- src/main/java/org/tat/gginl/api/services/CustomerService.java
+++ src/main/java/org/tat/gginl/api/services/CustomerService.java
@@ -5,8 +5,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.tat.gginl.api.domain.Customer;
-import org.tat.gginl.api.repository.CustomerRepository;
+import org.tat.gginl.api.domains.Customer;
+
 
 @Service
 public class CustomerService {
