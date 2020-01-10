052d0c39c04cb1fa986745efe30da808d574bc61
ျပည့္စံုဟိန္း <pyaesonehein1141991@gmail.com>
2020-01-10 11:57:55 +0630

    add Repository

---
+++ src/main/java/org/tat/gginl/api/repository/CustomerRepository.java
@@ -0,0 +1,12 @@
+package org.tat.gginl.api.repository;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.tat.gginl.api.domains.Customer;
+
+public interface CustomerRepository extends JpaRepository<Customer, String> {
+	List<Customer> findByRecorderCreatedDateBetweenOrRecorderUpdatedDateBetween(Date createdDate,Date updatedDate,Date createdDate1,Date updatedDate1);
+
+}
