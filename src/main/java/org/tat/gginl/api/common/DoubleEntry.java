301463df26c6496a496102919ea1aa46a05f2784
Aung Htet Paing <aunghtetpaing@hotmail.com>
2020-01-14 16:17:24 +0630

    a

---
+++ src/main/java/org/tat/gginl/api/common/DoubleEntry.java
@@ -0,0 +1,12 @@
+package org.tat.gginl.api.common;
+
+/**
+ * This enum is used for PaymentService and TLFBuilder only.
+ * 
+ * @author Ye Wint Aung
+ *
+ */
+public enum DoubleEntry {
+	DEBIT,
+	CREDIT;
+}
