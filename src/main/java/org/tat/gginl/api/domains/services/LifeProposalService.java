d2b5416c621bfa97f553ea172eeae5dc6e3c95f8
Aung Htet Paing <aunghtetpaing@hotmail.com>
2020-01-15 09:19:10 +0630

    aa

--- src/main/java/org/tat/gginl/api/domains/services/LifeProposalService.java
+++ src/main/java/org/tat/gginl/api/domains/services/LifeProposalService.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 
 import javax.transaction.SystemException;
 
+import org.apache.commons.collections.functors.TruePredicate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataAccessException;
